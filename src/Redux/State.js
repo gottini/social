452a20b2f888c529345a5c57c9fb5239d126ba13
gottini <gottini@users.noreply.github.com>
2020-05-19 12:20:27 +0300

    lesson 37

--- src/Redux/State.js
+++ src/Redux/State.js
@@ -1,81 +1,90 @@
-import {rerenderEntireTree} from "../render";
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Post 1', likesCount: 10},
+                {id: 2, message: 'Post 2', likesCount: 150}
+            ],
+            newPostText: ""
+        },
+        messagesPage: {
+            dialogs: [
+                {id: 1, name: 'User 1'},
+                {id: 2, name: 'User 2'},
+                {id: 3, name: 'User 3'},
+                {id: 4, name: 'User 4'},
+            ],
+            messages: [
+                {id: 1, message: 'Message 1'},
+                {id: 2, message: 'Message 2'}
+            ],
+            newMessageText: "New message"
+        },
+        navbarPage: {
+            friends: [
+                {
+                    id: 1,
+                    name: 'User 1',
+                    photo: 'https://i.insider.com/5ba15375e361c01c008b5cf7?width=839&format=jpeg'
+                },
+                {id: 2, name: 'User 2', photo: 'https://photos5.appleinsider.com/gallery/23569-29916-IMG_1538-xl.jpg'},
+                {
+                    id: 3,
+                    name: 'User 3',
+                    photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSvaMClzg6j-nwCrtaty5nECYh8TGZngGpzYFBohHmuY2Dhnlme&usqp=CAU'
+                },
+                {
+                    id: 4,
+                    name: 'User 4',
+                    photo: 'https://i.pinimg.com/474x/5c/3c/08/5c3c0807386ea51b2fc5a3ce9c839d1c.jpg'
+                },
+            ],
+        }
 
-let state = {
-    profilePage: {
-        posts: [
-            {id: 1, message: 'Post 1', likesCount: 10},
-            {id: 2, message: 'Post 2', likesCount: 150}
-        ],
-        newPostText: ""
     },
-    messagesPage: {
-        dialogs: [
-            {id: 1, name: 'User 1'},
-            {id: 2, name: 'User 2'},
-            {id: 3, name: 'User 3'},
-            {id: 4, name: 'User 4'},
-        ],
-        messages: [
-            {id: 1, message: 'Message 1'},
-            {id: 2, message: 'Message 2'}
-        ],
-        newMessageText: "New message"
+    getState() {
+        return this._state;
     },
-    navbarPage: {
-        friends: [
-            {
-                id: 1,
-                name: 'User 1',
-                photo: 'https://i.insider.com/5ba15375e361c01c008b5cf7?width=839&format=jpeg'
-            },
-            {id: 2, name: 'User 2', photo: 'https://photos5.appleinsider.com/gallery/23569-29916-IMG_1538-xl.jpg'},
-            {
-                id: 3,
-                name: 'User 3',
-                photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSvaMClzg6j-nwCrtaty5nECYh8TGZngGpzYFBohHmuY2Dhnlme&usqp=CAU'
-            },
-            {id: 4, name: 'User 4', photo: 'https://i.pinimg.com/474x/5c/3c/08/5c3c0807386ea51b2fc5a3ce9c839d1c.jpg'},
-        ],
-    }
+    _collSubscriber() {
+        console.log('State изменился')
+    },
+    addMessage() {
+        let newMessages = {
+            id: 3,
+            message: this._state.messagesPage.newMessageText,
+        };
 
-}
+        this._state.messagesPage.messages.push(newMessages);
+        this._state.messagesPage.newMessageText = "";
+        this._collSubscriber(this._state);
 
-export let addMessage = () => {
-    let newMessages = {
-        id: 3,
-        message: state.messagesPage.newMessageText,
-    };
+    },
+    updateNewMessageText(newText) {
+        this._state.messagesPage.newMessageText = newText;
+        this._collSubscriber(this._state);
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        };
 
-    state.messagesPage.messages.push(newMessages);
-    state.messagesPage.newMessageText = "";
-    rerenderEntireTree(state);
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = "";
+        this._collSubscriber(this._state);
 
-}
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._collSubscriber(this._state);
 
-export let updateNewMessageText = (newText) => {
-    state.messagesPage.newMessageText = newText;
-    rerenderEntireTree(state);
-}
+    },
+    subscribe(observer) {
 
-
-export let addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    };
-
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = "";
-    rerenderEntireTree(state);
-
-}
-
-export let updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-
+        this._collSubscriber = observer;
+    },
 }
 
 
-export default state;
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {rerenderEntireTree} from './render';
-import state from "./Redux/State";
+import store from "./Redux/State";
 
-rerenderEntireTree(state);
+
+let rerenderEntireTree = (state) => {
+
+    ReactDOM.render(<App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)}
+                         addMessage={store.addMessage.bind(store)}
+                         updateNewMessageText={store.updateNewMessageText.bind(store)}/>, document.getElementById('root'));
+
+}
+
+
+rerenderEntireTree(store.getState());
+
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
